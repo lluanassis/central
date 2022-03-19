7ad1bc5ce1ef95ca0a061f1f0b86c285ed45e6b8
Luan Assis <luan2lobo@gmail.com>
2022-03-18 23:53:27 -0300

    Seção 04 - REACT - Axios e Layout

--- front/pro-atividade-app/src/components/AtividadeForm.js
+++ front/pro-atividade-app/src/components/AtividadeForm.js
@@ -46,7 +46,6 @@ export default function AtividadeForm(props) {
 
   return (
     <>
-      <h1>Atividade {atividade.id !== 0 ? atividade.id : ""}</h1>
       <form className="row g-3" onSubmit={handleSubmit}>
         <div className="col-md-6">
           <label className="form-label">Título</label>
@@ -69,10 +68,10 @@ export default function AtividadeForm(props) {
             value={atividade.prioridade}
             className="form-select"
           >
-            <option defaultValue="0">Selecione...</option>
-            <option value="1">Baixa</option>
-            <option value="2">Normal</option>
-            <option value="3">Alta</option>
+            <option value="NaoDefinido">Selecione...</option>
+            <option value="Baixa">Baixa</option>
+            <option value="Normal">Normal</option>
+            <option value="Alta">Alta</option>
           </select>
         </div>
 
@@ -91,9 +90,9 @@ export default function AtividadeForm(props) {
 
         <div className="col-12 mt-0">
           {atividade.id === 0 ? (
-            <button className="btn btn-outline-secondary" type="submit">
+            <button className="btn btn-outline-sucess" type="submit">
               <i className="fas fa-plus me-2"></i>
-              Atividade
+              Salvar
             </button>
           ) : (
             <>
